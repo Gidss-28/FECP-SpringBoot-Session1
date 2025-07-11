38908b9ff31ccc59536c6852c5544ea6c81992dd
Gidss-28 <gideon-a.salangsang@gcash.com>
2025-07-11 14:40:46 +0800

    Added: created base classes Enclosure, Hospital, LectureHall, Shop, Product, ReceiptPrinter, Visitor, ZooModule

---
+++ src/main/java/org/example/LectureHall.java
@@ -0,0 +1,11 @@
+package org.example;
+
+public class LectureHall {
+    public static void showLectureTopics() {
+        // TODO: Print the lecture list
+    }
+
+    public static void attendLecture(String topic) {
+        // TODO: Show the lecture content
+    }
+}
