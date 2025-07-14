1e40fc4ea9f8e364115168428324910cecaa2d0d
Gidss-28 <gideon-a.salangsang@gcash.com>
2025-07-14 17:32:37 +0800

    Refined HandlerModule integration and interaction flow

--- src/main/java/org/example/Person/Staff/Handler.java
+++ src/main/java/org/example/Person/Staff/Handler.java
@@ -26,7 +26,6 @@ public class Handler <T extends Animal> extends Person{
 
     public void examineToVet(String animalName) {
         T animal = assignedEnclosure.getAnimal(animalName);
-        // TODO: implement transfer of animal to hospital
         System.out.println(animal.getName() + " admitted at " + LocalDateTime.now().format(DateTimeFormatter.ofPattern("dd-MM-yyyy HH:mm:ss")));
     }
 
